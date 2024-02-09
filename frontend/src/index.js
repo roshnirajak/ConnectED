d6bcffe372c76d36bc4c3e449bdc1c7d689f5fce
roshnirajak <imroshni3@gmail.com>
2024-02-09 17:01:23 +0530

    enabled strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StrictMode } from 'react';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <StrictMode>
     <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
